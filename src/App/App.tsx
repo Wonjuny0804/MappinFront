decdaf0cb73aa41f6b1d517cf2630a6e3df327b2
wonjun <enkor@enkorui-MacBookPro.local>
2021-07-31 08:22:01 +0900

    First setup

---
+++ src/App/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="App">
+      okay I am working now
+
+      <p>
+        There might be something wrong with the system I think
+      </p>
+    </div>
+  );
+}
+
+export default App;
