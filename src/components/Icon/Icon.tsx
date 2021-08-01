485e8460f951535f107c5968b7bfd71c31ad0707
wonjun <enkor@enkorui-MacBookPro.local>
2021-08-01 23:40:03 +0900

    Create components

---
+++ src/components/Icon/Icon.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { ReactComponent as Logo } from "./assets/로고.svg";
+
+function Icon() {
+    return (
+        <div>
+           <Logo /> 
+        </div>
+    )
+}
+
+export default Icon
