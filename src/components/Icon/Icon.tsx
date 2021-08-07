2c1a7afd7297609bf3951a294115e95b6ce17e5f
Wonjun <wonwonjun@gmail.com>
2021-08-07 18:09:08 +0900

    Add prev, next icon svgs

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import { ReactComponent as Logo } from "./assets/로고.svg";
 import { ReactComponent as NomadMap } from "./assets/Nomads_Map.svg";
+import { ReactComponent as Prev } from "./assets/prev.svg";
+import { ReactComponent as Next } from "./assets/next.svg";
 
 interface IconProps {
-    type: "Logo" | "NomadMap";
+    type: "Logo" | "NomadMap" | "Prev" | "Next";
 }
 
 function Icon({ type }: IconProps):JSX.Element {
 
     const dataObject = {
         Logo: <Logo />,
-        NomadMap: <NomadMap />
+        NomadMap: <NomadMap />,
+        Prev: <Prev />,
+        Next: <Next />
     }
 
     return (
