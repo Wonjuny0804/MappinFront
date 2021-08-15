51d8606fd0dda36cde7c82caa03e131e37478ab4
Wonjun <wonwonjun@gmail.com>
2021-08-15 18:11:35 +0900

    Add blur event to Input

--- src/components/SearchInput/SearchInput.tsx
+++ src/components/SearchInput/SearchInput.tsx
@@ -1,9 +1,6 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef } from 'react';
 import { Icon } from '..';
 import styles from "./SearchInput.module.scss";
-
-interface SearchInputProps extends React.Component<HTMLInputElement> {
-}
 
 function SearchInput():JSX.Element {
 
@@ -16,23 +13,16 @@ function SearchInput():JSX.Element {
         inputRef.current && inputRef.current.focus();
     }
 
-    useEffect(() => {
-        
-        inputRef.current?.addEventListener("blur", (e: React.BlurEvent<HTMLInputElement>):void => {
-            if (target) {
-                
-                if (target.value === "")
+    const handleInputBlur = (e: React.FocusEvent<HTMLInputElement>): void => {
+        if (e.target.value === "" && labelRef.current) labelRef.current.style.display = "block";
+    }
 
-            }
-        });
-
-    }, [])
 
     return (
         <div className={styles.inputWrapper} ref={wrapperRef} onClick={handleWrapperClick}>
             <Icon type="Search" />
             <label htmlFor="SearchInput" className={styles.label} ref={labelRef}>가고 싶은 곳을 검색해 보세요</label>
-            <input type="search" id="SearchInput" name="search" minLength={2} className={styles.input} ref={inputRef}/>
+            <input type="search" id="SearchInput" name="search" minLength={2} className={styles.input} ref={inputRef} onBlur={handleInputBlur}/>
         </div>
     )
 }
