bcc3356669530ef18455f209b62f8cf9fecd45c8
Wonjun <wonwonjun@gmail.com>
2021-08-16 21:22:43 +0900

    Create component RecommendCard

---
+++ src/components/RecommendCard/RecommendCard.tsx
@@ -0,0 +1,23 @@
+import React, { useEffect, useRef } from "react";
+import styles from "./RecommendCard.module.scss";
+
+interface RecommendCardProps {
+    cardTitle: string
+    backgroundURL: string;
+}
+
+function RecommendCard({ cardTitle, backgroundURL }: RecommendCardProps ):JSX.Element {
+
+    const buttonRef = useRef<HTMLButtonElement>(null);
+
+    useEffect(() => {
+        if (buttonRef.current) {
+            buttonRef.current.style.background = ` linear-gradient(0deg, rgba(52, 52, 52, 0.3), rgba(52, 52, 52, 0.3)), url(${backgroundURL})`;
+            buttonRef.current.style.backgroundPosition = "center";
+        }
+    }, [])
+
+    return <button ref={buttonRef} type="submit" className={styles.card}>{cardTitle}</button>
+}
+
+export default RecommendCard;
