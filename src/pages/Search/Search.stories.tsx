9fb054fd50e0b66afdfa65783ccdf5f7233382c5
Wonjun <wonwonjun@gmail.com>
2021-08-21 22:42:32 +0900

    Modify and create example for Search page

--- src/pages/Search/Search.stories.tsx
+++ src/pages/Search/Search.stories.tsx
@@ -1,1 +1,25 @@
 import React from "react";
+import { Meta } from "@storybook/react";
+import Search from "./Search";
+import { NavBar } from "../../components";
+
+export default {
+  component: Search,
+  title: "Search",
+} as Meta;
+
+export const SearchScreen = (): JSX.Element => {
+  return (
+  <body
+    style={
+      { 
+        minHeight: "100vh", 
+        padding: "0", 
+        border: "1px solid black" 
+      }}
+  >
+    <NavBar />
+    <Search />
+  </body>
+  );
+}
--- src/pages/Search/Search.tsx
+++ src/pages/Search/Search.tsx
@@ -1,24 +1,54 @@
-import React from 'react';
-import { SearchInput } from "../../components/";
+import React, { useState, useEffect } from 'react';
+import { SearchInput, RecommendCard, RecommendKeyword, IconButton } from "../../components/";
+import styles from "./Search.module.scss";
+import { useHistory } from "react-router-dom";
 
 function Search() {
+    const [recommendWords, setRecommendWords] = useState<Array<{ name: string, url: string}>>([]);
+    const [keywords, setKeywords] = useState<Array<Array<string>>>([]);
+
+    const history = useHistory();
+
+    const handleOnGoBack = ():void => {
+      history.goBack();
+    }
+  
+
+    useEffect(() => {
+
+    }, []);
+
+
     return (
-        <main>
-            <div>
+        <main className={styles.pageLayout}>
+            <div className={styles.header}>
                 <h1>관광지 검색</h1>
                 <h2>꼭 가고 싶은 장소 검색을 통해 일정을 추천받아보세요.</h2>
             </div>
             <SearchInput />
-            <section>
+            <section className={styles.recommendWord}>
               <h2>추천 검색어</h2>
               <div>
-
+                {recommendWords 
+                && recommendWords.map((word) => 
+                <RecommendCard 
+                  cardTitle={word.name} 
+                  backgroundURL={word.url}
+                />)}
               </div>
             </section>
-            <section>
+            <section className={styles.recommendKeyword}>
               <h2>추천 키워드</h2>
-              <div></div>
+              <div>
+                {keywords 
+                && keywords.map((word) => 
+                <RecommendKeyword
+                  keywords={word} 
+                  // onClick={}
+                />)}
+              </div>
             </section>
+            <IconButton type="button" onClick={handleOnGoBack} icon="Prev" className={styles.goBack}/>
         </main>
     )
 }
